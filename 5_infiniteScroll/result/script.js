ee3d3b656ecadacbf4d55e6d3cef641ec3747ad5
okwonye@gmail.com <okwonye@gmail.com>
2024-03-12 11:54:19 +0900

    - 무한스크롤 비동기로 불러오기 async-await 으로 구현 - scroll 핸들러와 scrollTop, scrollHeight, clientHeight 를 활용해 조건문 및 이벤트 핸들러 작성

--- 5_infiniteScroll/result/script.js
+++ 5_infiniteScroll/result/script.js
@@ -4,4 +4,79 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  let page = 1
+  const limit = 10
+  const $posts = get('.posts')
+  const end = 100
+  let total = 10
+
+  const $loader = get('.loader')
+
+  const getPost = async () => {
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
+    const response = await fetch(API_URL)
+    if (!response.ok) {
+      throw new Error('에러')
+    }
+    return await response.json()
+  }
+
+  const showPosts = (posts) => {
+    posts.forEach((post) => {
+      const $post = document.createElement('div')
+      $post.classList.add('post')
+      $post.innerHTML = `
+        <div class="header">
+          <div class="id">${post.id}</div>
+          <div class="title">${post.title}</div>
+        </div>
+        <div class="body">
+          ${post.body}
+        </div>
+      `
+      $posts.appendChild($post)
+    })
+  }
+
+  const showLoader = () => {
+    $loader.classList.add('show')
+  }
+
+  const hideLoader = () => {
+    $loader.classList.remove('show')
+  }
+
+  const loadPosts = async (page, limit) => {
+    showLoader()
+    try{
+      const response = await getPost(page, limit)
+      showPosts(response)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      hideLoader()
+    }
+  }
+
+  const onScroll = () => {
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement
+
+    if (total === end) {
+      window.removeEventListener('scroll', handleScroll)
+      return
+    }
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page++
+      total += 10
+      loadPosts()
+      return
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    loadPosts()
+    window.addEventListener('scroll', onScroll)
+  })
 })()
