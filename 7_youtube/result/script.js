5d64bea896826005198e014140769339e2d349f4
okwonye@gmail.com <okwonye@gmail.com>
2024-03-15 19:06:15 +0900

    - 유튜브 비디오 컨트롤러 구현 - play / pause / replay / stop / mute / fullScreen - 참고 강의에서 다른 코드 작성법이지만 같은 기능을 하는 중복 이벤트가 구현되어 참고용으로 주석처리만 해두고 최종 기능 하나만 남김

--- 7_youtube/result/script.js
+++ 7_youtube/result/script.js
@@ -8,6 +8,15 @@
   const $list = getAll('.contents.list figure');
   const $searchButton = get('.btn_search');
 
+  const $player = get('.view video');
+  const $btnPlay = get('.js-play');
+  const $btnReplay = get('.js-replay');
+  const $btnStop = get('.js-stop');
+  const $btnMute = get('.js-mute');
+  const $progress = get('.js-progress');
+  const $volume = get('.js-volume');
+  const $fullScreen = get('.js-fullScreen');
+  
   const init = () => {
     $search.addEventListener('keyup', search);
     $searchButton.addEventListener('click', search);
@@ -30,6 +39,8 @@
         getListPage();
       }
     })
+
+    viewPageEvent();
   }
 
   const search = () => {
@@ -77,6 +88,101 @@
   const getListPage = () => {
     get('.list').style.display = 'flex';
     get('.view').style.display = 'none';
+  }
+
+  const buttonChange = (btn, value) => {
+    btn.innerText = value;
+  }
+
+  const viewPageEvent = () => {
+    $volume.addEventListener('change', (e) => {
+      $player.volume = e.target.value;
+    })
+
+    $player.addEventListener('timeupdate', setProgress)
+    // $player.addEventListener('play', buttonChange($btnPlay, 'pause'))
+    // $player.addEventListener('pause', buttonChange($btnPlay, 'play'))
+    // $player.addEventListener('volumeChange', () => {
+    //   $player.muted 
+    //   ? buttonChange($btnMute, 'unmute')
+    //   : buttonChange($btnMute, 'mute')
+    // })
+    $player.addEventListener('ended', $player.pause())
+    $progress.addEventListener('click', getCurrent)
+    $btnPlay.addEventListener('click', playVideo)
+    $btnStop.addEventListener('click', stopVideo)
+    $btnReplay.addEventListener('click', replayVideo)
+    $btnMute.addEventListener('click', mute)
+    $fullScreen.addEventListener('click', fullScreen)
+  }
+
+  const getCurrent = (e) => {
+    // e.offsetX => 이벤트 발생 시 progress 바 위치
+    // $progress.offsetWidth => progress 바 전체 넓이
+    let percent = e.offsetX / $progress.offsetWidth;
+    // $player.duration => 비디오 재생 시간(초 단위)
+    $player.currentTime = percent * $player.duration;
+    e.target.value = Math.floor(percent / 100);
+  }
+
+  const setProgress = () => {
+    let percentage = Math.ceil((100 / $player.duration) * $player.currentTime)
+    $progress.value = percentage;
+  }
+
+  const playVideo = () => {
+    if($player.paused || $player.ended) {
+      buttonChange($btnPlay, 'Pause');
+      $player.play();
+    } else {
+      buttonChange($btnPlay, 'Play')
+      $player.pause();
+    }
+  }
+
+  const stopVideo = () => {
+    $player.pause();
+    $player.currentTime = 0
+    buttonChange($btnPlay, 'play')
+  }
+
+  const resetPlayer = () => {
+    // $progress.value = 0
+    $player.currentTime = 0
+    buttonChange($btnPlay, 'play')
+  }
+
+  const replayVideo = () => {
+    resetPlayer()
+    $player.play()
+    buttonChange($btnPlay, 'Pause')
+  }
+
+  const mute = () => {
+    if($player.muted) {
+      buttonChange($btnMute, 'mute')
+      $player.muted = false;
+    } else {
+      buttonChange($btnMute, 'unmuted')
+      $player.muted = true;
+    }
+  }
+
+  const fullScreen = () => {
+    if($player.requestFullscreen) {
+      // document.fullscreenElement => 전체화면 사용중인지 여부
+      if(document.fullscreenElement) {
+        document.cancelFullScreen()
+      } else {
+        $player.requestFullscreen()
+      }
+    } else if($player.msRequestFullscreen) {
+      if(document.msRequestFullscreen) {
+        document.msExitFullscreen()
+      } else {
+        alert('Not Supported')
+      }
+    }
   }
 
   init()
