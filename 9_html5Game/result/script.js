a0989e2f04a56843aca37b82f93a51ec5007993a
okwonye@gmail.com <okwonye@gmail.com>
2024-03-15 21:19:58 +0900

    - 지렁이 게임 생성 - js에서 color style 지정 - color set 은 옵션화로 가져다 쓰는 방식 - play 클릭시 color 적용 및 snake, food 생성

--- 9_html5Game/result/script.js
+++ 9_html5Game/result/script.js
@@ -3,7 +3,7 @@
 
   const get = (target) => document.querySelector(target)
 
-  const $canvas = get('convas')
+  const $canvas = get('canvas')
   const ctx = $canvas.getContext('2d')
 
   const $score = get('.score')
@@ -33,7 +33,79 @@
   }
 
   const init = () => {
+    // document.addEventListener('keydown', (event) => {
+    //   if(!/Arrow/gi.test(event.key)) {
+    //     return
+    //   }
+    //   event.preventDefault()
+    //   const direction = getDirection(event.key)
+    //   if(!isDirectionCorrect(direction)) {
+    //     return
+    //   }
+    //   option.direction = direction
+    // })
 
+    $play.onclick = () => {
+      if(option.gameEnd) {
+        option= {
+          highscore: localStorage.getItem('score') || 0,
+          gameEnd: false,
+          direction: 2,
+          snake: [
+            {x: 10, y: 10, direction: 2},
+            {x: 10, y: 20, direction: 2},
+            {x: 10, y: 30, direction: 2},
+          ],
+          food: {x: 0, y: 0},
+          score: 0,
+        }
+        $score.innerHTML = '점수 : 0점'
+        $highscore.innerHTML = `최고점수: ${option.highscore}점`
+        // randomFood()
+        window.requestAnimationFrame(play)
+      }
+    }
+  }
+
+  const buildBoard = () => {
+    ctx.fillStyle = colorSet.board
+    ctx.fillRect(0, 0, 300, 300)
+  }
+
+  const buildSnake = (ctx, x, y, head = false) => {
+    ctx.fillStyle = head ? colorSet.snakeHead : colorSet.snakeBody
+    ctx.fillRect(x, y, 10, 10)
+  }
+
+  const buildFood = (ctx, x, y) => {
+    ctx.beginPath()
+    ctx.fillStyle = colorSet.food
+    ctx.arc(x + 5, y + 5, 5, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  const setSnake = () => {
+    for (let i = option.snake.length -1; i >= 0; --i) {
+      buildSnake(ctx, option.snake[i].x, option.snake[i].y, i === 0)
+    }
+  }
+
+  const play = (timestamp) => {
+    start++
+    if(option.gameEnd) return
+    if(timestamp - start > 1000 /10) {
+      buildBoard()
+      buildFood(ctx, option.food.x, option.food.y)
+      setSnake()
+      start = timestamp
+    }
+    window.requestAnimationFrame(play)
+    // if(isGameOver()) {
+    //   option.gameEnd = true
+    //   setHighScore()
+    //   alert('게임오버!')
+    //   return
+    // }
   }
 
   init()
