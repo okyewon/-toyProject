b95fbc39a9172e3141a033039218bd21b797d089
okwonye@gmail.com <okwonye@gmail.com>
2024-03-15 20:59:57 +0900

    - todolist 추천 기능 추가 - recommend data 추가 및 클릭시 이벤트 핸들러 생성 - todolist 제목 클릭시 수정 가능 - 수정 중 enter 키로 수정 완료 / esc 키로 수정 취소
    - 지렁이게임 기본 셋팅

--- 8_todolistRecommend/result/script.js
+++ 8_todolistRecommend/result/script.js
@@ -65,8 +65,9 @@
   }
 
   const createTodoElement = (item) => {
-    const { id, content, completed } = item
+    const { id, content, completed, recommended } = item
     const isChecked = completed ? 'checked' : ''
+    const isRecommended = recommended ? 'active' : ''
     const $todoItem = document.createElement('div')
     $todoItem.classList.add('item')
     $todoItem.dataset.id = id
@@ -77,10 +78,14 @@
                 class='todo_checkbox'
                 ${isChecked}
               />
-              <label>${content}</label>
+              <label class="title">${content}</label>
               <input type="text" value="${content}" />
             </div>
             <div class="item_buttons content_buttons">
+              <button class="todo_recommend_button ${isRecommended}">
+                <i class="far fa-star"></i>
+                <i class="fas fa-star"></i>
+              </button>
               <button class="todo_edit_button">
                 <i class="far fa-edit"></i>
               </button>
@@ -154,6 +159,21 @@
       .catch((error) => console.error(error.message))
   }
 
+  const recommendTodo = (e) => {
+    if(!e.target.classList.contains('todo_recommend_button')) return;
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+    const recommended = !e.target.classList.contains('active')
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({recommended}),
+    })
+      .then((response) => response.json())
+      .then(getTodos)
+      .catch((error) => console.error(error.message))
+  }
+
   const changeEditMode = (e) => {
     const $item = e.target.closest('.item')
     const $label = $item.querySelector('label')
@@ -162,7 +182,7 @@
     const $editButtons = $item.querySelector('.edit_buttons')
     const value = $editInput.value
 
-    if (e.target.className === 'todo_edit_button') {
+    if (e.target.className === 'todo_edit_button' || e.target.className === 'title') {
       $label.style.display = 'none'
       $editInput.style.display = 'block'
       $contentButtons.style.display = 'none'
@@ -172,7 +192,7 @@
       $editInput.value = value
     }
 
-    if (e.target.className === 'todo_edit_cancel_button') {
+    if (e.target.className === 'todo_edit_cancel_button' || e.keyCode === 27) {
       $label.style.display = 'block'
       $editInput.style.display = 'none'
       $contentButtons.style.display = 'block'
@@ -182,20 +202,21 @@
   }
 
   const editTodo = (e) => {
-    if (e.target.className !== 'todo_edit_confirm_button') return
-    const $item = e.target.closest('.item')
-    const id = $item.dataset.id
-    const $editInput = $item.querySelector('input[type="text"]')
-    const content = $editInput.value
-
-    fetch(`${API_URL}/${id}`, {
-      method: 'PATCH',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ content }),
-    })
-      .then((response) => response.json())
-      .then(getTodos)
-      .catch((error) => console.error(error.message))
+    if (e.target.className === 'todo_edit_confirm_button' || e.keyCode === 13) {
+      const $item = e.target.closest('.item')
+      const id = $item.dataset.id
+      const $editInput = $item.querySelector('input[type="text"]')
+      const content = $editInput.value
+  
+      fetch(`${API_URL}/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({ content }),
+      })
+        .then((response) => response.json())
+        .then(getTodos)
+        .catch((error) => console.error(error.message))
+    }
   }
 
   const removeTodo = (e) => {
@@ -220,8 +241,11 @@
     $form.addEventListener('submit', addTodo)
     $todos.addEventListener('click', toggleTodo)
     $todos.addEventListener('click', changeEditMode)
+    $todos.addEventListener('keydown', changeEditMode)
     $todos.addEventListener('click', editTodo)
+    $todos.addEventListener('keydown', editTodo)
     $todos.addEventListener('click', removeTodo)
+    $todos.addEventListener('click', recommendTodo)
   }
 
   init()
--- 9_html5Game/result/script.js
+++ 9_html5Game/result/script.js
@@ -1,3 +1,40 @@
 ;(function () {
   'use strict'
+
+  const get = (target) => document.querySelector(target)
+
+  const $canvas = get('convas')
+  const ctx = $canvas.getContext('2d')
+
+  const $score = get('.score')
+  const $highscore = get('.highscore')
+  const $play = get('.js-play')
+
+  //옵션화
+  const colorSet = {
+    board: 'rgb(20, 105, 38)',
+    snakeHead: 'rgba(229, 65, 120, 0.929)',
+    snakeBody: 'rgba(153, 206, 244, 0.498)',
+    food: 'rgb(66, 187, 103)',
+  }
+
+  let start = 0;
+  let option = {
+    highscore: localStorage.getItem('score') || 0,
+    gameEnd: true,
+    direction: 2, // 시계방향으로 4개의 모서리 중 2번 위치
+    snake: [
+      {x: 10, y: 10, direction: 2},
+      {x: 10, y: 20, direction: 2},
+      {x: 10, y: 30, direction: 2},
+    ],
+    food: {x: 0, y: 0},
+    score: 0,
+  }
+
+  const init = () => {
+
+  }
+
+  init()
 })()
