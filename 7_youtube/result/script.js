584b237175b642594c3c02235ce9b53221ae2aaf
okwonye@gmail.com <okwonye@gmail.com>
2024-03-15 17:39:55 +0900

    - 유튜브 뷰페이지 진입시 hash 사용 - hash 에 view 내용이 없으면 list 페이지 노출

--- 7_youtube/result/script.js
+++ 7_youtube/result/script.js
@@ -11,24 +11,39 @@
   const init = () => {
     $search.addEventListener('keyup', search);
     $searchButton.addEventListener('click', search);
+
     for (let index = 0; index < $list.length; index++) {
       const $target = $list[index].querySelector('picture');
       $target.addEventListener('mouseover', onMouseOver);
       $target.addEventListener('mouseout', onMouseOut);
     }
+
+    for (let index = 0; index < $list.length; index++) {
+      $list[index].addEventListener('click', hashChange)
+    }
+
+    window.addEventListener('hashchange', () => {
+      const isView = -1 < window.location.hash.indexOf('view');
+      if(isView) {
+        getViewPage();
+      } else {
+        getListPage();
+      }
+    })
   }
 
   const search = () => {
     let searchText = $search.value.toLowerCase();
+
     for (let index = 0; index < $list.length; index++) {
       const $target = $list[index].querySelector('strong');
-      const text = $target.textContent.toLowerCase()
+      const text = $target.textContent.toLowerCase();
+
       if(-1 < text.indexOf(searchText)) {
         $list[index].style.display = 'flex';
       } else {
         $list[index].style.display = 'none';
       }
-      
     }
   }
 
@@ -40,6 +55,28 @@
   const onMouseOut = (e) => {
     const webpPlay = e.target.parentNode.querySelector('source');
     webpPlay.setAttribute('srcset', './assets/sample.jpg');
+  }
+
+  const hashChange = (e) => {
+    e.preventDefault();
+    const parentNode = e.target.closest('figure');
+    const viewTitle = parentNode.querySelector('strong').textContent;
+    window.location.hash = `view&${viewTitle}`;
+  }
+
+  const getViewPage = () => {
+    const viewTitle = get('.view strong');
+    // 인코딩 된 문자열을 그대로 넣지 않고 decodeURI 사용
+    const urlTitle = decodeURI(window.location.hash.split('&')[1]);
+    viewTitle.innerText = urlTitle;
+
+    get('.list').style.display = 'none';
+    get('.view').style.display = 'flex';
+  }
+
+  const getListPage = () => {
+    get('.list').style.display = 'flex';
+    get('.view').style.display = 'none';
   }
 
   init()
