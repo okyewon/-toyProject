70b265a8493b1fa8ec2ee006a9f6c437eba1d06b
okwonye@gmail.com <okwonye@gmail.com>
2024-03-18 16:23:59 +0900

    - 모달창 구현 - 모달 팝업 오픈시 배경 스크롤 제한 - 버튼 또는 모달창 밖 배경 클릭시 모달창 닫기

--- 10_modal_popup/result/script.js
+++ 10_modal_popup/result/script.js
@@ -3,4 +3,31 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button')
+  const $modal = get('.modal')
+  const $body = get('body')
+  const $modalCancelButton = get('.modal_button.cancel')
+  const $modalConfirmButton = get('.modal_button.confirm')
+
+  const toggleModal = () => {
+    $modal.classList.toggle('show')
+    $body.classList.toggle('scroll_lock')
+  }
+
+  $button.addEventListener('click', () => {
+    toggleModal()
+  })
+
+  $modalCancelButton.addEventListener('click', () => {
+    toggleModal()
+  })
+
+  $modalConfirmButton.addEventListener('click', () => {
+    toggleModal()
+  })
+
+  window.addEventListener('click', (e) => {
+    if(e.target === $modal) toggleModal()
+  })
 })()
