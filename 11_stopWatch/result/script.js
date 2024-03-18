c4f6ff18e04422970924bdaa850810f2abdd381f
okwonye@gmail.com <okwonye@gmail.com>
2024-03-18 16:41:42 +0900

    - 스톱워치 start 버튼 구현 - 단위 별 숫자는 2자릿수를 넘어가지 않도록 제한

--- 11_stopWatch/result/script.js
+++ 11_stopWatch/result/script.js
@@ -4,4 +4,58 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  class Stopwatch {
+    constructor(element) {
+      this.timer = element;
+      this.interval = null;
+      this.defaultTime = '00:00.00';
+      this.startTime = 0;
+      this.elapsedTime = 0;
+    }
+
+    addZero(number) {
+      if(number < 10) {
+        return '0' + number
+      }
+      if(number > 99) {
+        return number.toString().slice(0, -1)
+      }
+      return number
+    }
+
+    timeToString(time) {
+      const date = new Date(time)
+      const minutes = date.getUTCMinutes()
+      const seconds = date.getUTCSeconds()
+      const millisecond = date.getUTCMilliseconds()
+      return `${this.addZero(minutes)}:${this.addZero(seconds)}.${this.addZero(millisecond)}`
+    }
+
+    print(text) {
+      this.timer.innerHTML = text
+    }
+
+    startTimer() {
+      this.elapsedTime = Date.now() - this.startTime
+      const time = this.timeToString(this.elapsedTime)
+      this.print(time)
+    }
+
+    start() {
+      this.startTime = Date.now() - this.elapsedTime;
+      this.interval = setInterval(this.startTimer.bind(this), 10)
+    }
+
+    stop() {}
+    reset() {}
+  }
+
+  const $startButton = get('.timer_button.start')
+  const $timer = get('.timer')
+  const stopwatch = new Stopwatch($timer)
+
+  $startButton.addEventListener('click', () => {
+    stopwatch.start()
+  })
 })()
